f0bf819a0496751928249d1a5feca64b1703f643
Scott Null <snull@contactsolutions.com>
2015-11-24 16:25:51 -0500

    added comments

--- MQTTExample/MQTTViewController.h
+++ MQTTExample/MQTTViewController.h
@@ -14,6 +14,13 @@
 // to put some text in the Subscribed Text box:
 //   curl -X PUT --data "put any message you want to show here" http://eclipse.mqttbridge.com/%2FMQTTExample%2FLED
 
+//  to send test messages to a topic from the command line to localhost:
+//  mosquitto_pub -m 0 -t MQTTExample/LED
+//  mosquitto_pub -m 1 -t MQTTExample/LED
+//  mosquitto_pub -m "hey batter batter swing" -t MQTTExample/LED
+
+
+
 #import <UIKit/UIKit.h>
 
 @interface MQTTViewController : UIViewController
